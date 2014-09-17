969aacbb966b8cd4e4183d27e4ed6a4cff9640d1
Dr. Robert Winkler <robert.winkler@ira.cinvestav.mx, robert.winkler@bioprocess.org>
2014-09-17 12:33:46 -0500

    first MSI scanning script

--- main/mz-image.R
+++ main/mz-image.R
@@ -1,3 +1,11 @@
+# This script generates an image of a selected m/z range
+# Please enter the desired mass range in  the line mzrangeindex <- (specmatrix[,1] > 820 & specmatrix[,1] < 840)
+# Further important parameters are:
+# * Location of the imzMLConverter (.jaddClassPath)
+# * Location of imzML file
+
+# Clear Workspace
+rm(list=ls())
 #set working directory to source file
 setwd("~/SRC/MSI.R/main")
 
@@ -15,6 +23,7 @@ library(latticeExtra)
 
 # Load the imzML data
 filename <- "/home/rob/SRC/MSI.R/SampleData/test.imzML"
+#filename <- "/home/rob/MS-DATA/LTP-MSI-chilli/ltpms-chilli.imzML"
 imzML <- J("imzMLConverter.ImzMLHandler")$parseimzML(filename)
 imagewidth <- J(imzML, 'getWidth')
 imageheight <- J(imzML, 'getHeight')
@@ -24,6 +33,7 @@ colnames(intmatrix) <- seq(1:imageheight)
 
 x <- 1
 y <- 1
+specno <-1
 
 while (y<=imageheight)
 {  
@@ -44,11 +54,15 @@ if (decisionvalue>"1"){
 mzrangeints<-specmatrix[mzrangeindex, ]
 mzpixintensity<-sum(mzrangeints[,2])}
 intmatrix[x,y]<-mzpixintensity
+print(specno)
 x=x+1
+specno=specno+1
 }
 y=y+1
 }
 
 png("mzimage.png")
-levelplot(intmatrix,main="Intensity m/z",xlab="x/ pixel",ylab="y/ pixel",scales = list(draw = FALSE),contour=TRUE,pretty=TRUE,col.regions = terrain.colors(100))
+print(levelplot(intmatrix,main="Intensity m/z",xlab="x/ pixel",ylab="y/ pixel",scales = list(draw = FALSE),contour=TRUE,pretty=TRUE,col.regions = terrain.colors(100)))
 dev.off()
+
+print("mz image saved to file.")
