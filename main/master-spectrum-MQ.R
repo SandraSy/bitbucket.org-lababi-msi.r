b77366e451b330c01356a05a5f6dfb8ee81c8874
Sebastian Gibb <mail@sebastiangibb.de>
2015-04-22 21:35:19 +0200

    replace some self-written functions by their MALDIquant counterpart

--- main/master-spectrum-MQ.R
+++ main/master-spectrum-MQ.R
@@ -1,4 +1,5 @@
-# This script summarizes the x (e.g. 100) most intense peaks/ point of each spectrum in one single spectrum
+# This script summarizes the x (e.g. 100) most intense peaks/point of each
+# spectrum in one single spectrum
 # The increasing number indicates the number of processed spectra
 # The final spectrum is exported into a .mzML file and printed to a .tiff
 # A density distribution map is printed to a publication quality .tiff
@@ -6,7 +7,7 @@
 # Please adjust the paramenters for peak picking (e.g. signal/noise) and spectrum zoom below
 
 # You can change the number of selected peaks with (this would be the setting for 100 signals):
-#if (length(newspec)>"200"){        //2x the number of peaks 
+#if (length(newspec)>"200"){        //2x the number of peaks
 #  newspec <- newspec[1:100,]}      //1:x strongest signals
 
 # Clear Workspace
@@ -18,68 +19,59 @@ library("MALDIquant")
 library("MALDIquantForeign")
 
 # Load the imzML data
-filename <- "../data/ltpmsi-chilli.imzML"
+filename <- file.path("..", "data", "ltpms-chilli.imzML")
 
-imagespectra <- importImzMl(filename, centroided=TRUE)
-#plotImsSlice(imagespectra)
-elementsimzML<-length(imagespectra)
-
-specno <-1
-
-while (specno<=elementsimzML)
-{
-    mzs <- mass(imagespectra[[specno]])
-    counts <- intensity(imagespectra[[specno]])
-    newspec  <- cbind(mzs, counts)
-    newspec <- newspec[order(-counts),]
-    if (length(newspec)>"200"){
-    newspec <- newspec[1:100,]}
-    
-    mzs<-newspec[,1]
-    counts<-newspec[,2]
-    
-    if(exists("mzsum")=="TRUE"){mzsum<-c(mzsum,mzs)}
-    if(exists("countsum")=="TRUE"){countsum<-c(countsum,counts)}
-    if(exists("mzsum")=="FALSE"){mzsum<-mzs}
-    if(exists("countsum")=="FALSE"){countsum<-counts}
-    specmatrix <- cbind(mzsum, countsum)  
-    print(specno)
-  specno=specno+1
+# Define a new function to find and return the highest n peaks in each spectrum.
+topN <- function(peaklist, n=100) {
+  lapply(peaklist, function(x) {
+    x[sort.int(intensity(x), decreasing=TRUE, index.return=TRUE)$ix[1:n]]
+  })
 }
 
-mzsumdensity<-density(mzsum, bw=0.01)
+imagespectra <- importImzMl(filename, centroided=TRUE)
 
-# printing spectrum. format options could be e.g. png, eps, tiff, pdf. You can control the resolution with res
-tiff(filename="spectrumdensity.tiff",res=1200,compression="lzw",height=200,width=200,units="mm")
-plot(mzsumdensity,,xlab="m/z",ylab="density",main="m/z density distribution")
-polygon(mzsumdensity, col="grey") 
-dev.off()
+imagespectra <- topN(imagespectra, n=100)
 
-# aggregate spectrum data
-specmatrix<-aggregate(countsum ~ mzsum, data=specmatrix, FUN=sum)
-specmatrix<-specmatrix[ order(specmatrix[,1]), ]
-s <- list(createMassSpectrum(mass=specmatrix[,1], intensity=specmatrix[,2]))
+mzdensity <- density(sapply(imagespectra, mass), bw=0.01)
 
-# print master spectrum
-tiff(filename="masterspectrum.tiff",res=1200,compression="lzw",height=200,width=200,units="mm")
-plot(s[[1]], xlab="m/z")
-dev.off()
-
-# export a single mzML spectrum
-exportMzMl(s[[1]], force=TRUE, file="masterspectrum.mzML")
+## as needs MALDIquant 1.11.4 from github: install_github("sgibb/MALDIquant")
+## otherwise you could use:
+## peaks <- mergeMassPeaks(imagespectra.sg, method="sum")
+## masterspectrum <- createMassSpectrum(mass(peaks), intensity(peaks))
+masterspectrum <- as(mergeMassPeaks(imagespectra, method="sum"),
+                     "MassSpectrum")
 
 # peak picking and export to a peaklist.csv file
-peaks <-  detectPeaks(s, SNR = 3)
+peaks <- detectPeaks(masterspectrum, SNR=3)
 
-tiff(filename="spectrum-with-peaks.tiff",res=1200,compression="lzw",height=200,width=200,units="mm")
-plot(s[[1]],xlim=c(40, 550), xlab="m/z")
-points(peaks[[1]],pch=18)
+
+# printing spectrum. format options could be e.g. png, eps, tiff, pdf.
+# You can control the resolution with res
+tiff(filename="spectrumdensity.tiff", res=1200, compression="lzw",
+     height=200, width=200, units="mm")
+plot(mzdensity, xlab="m/z", ylab="density", main="m/z density distribution")
+polygon(mzdensity, col="grey")
 dev.off()
 
-# export of peaklist
-peaklist<-intensityMatrix(peaks)
-tpeaklist<-t(peaklist)
-colnames(tpeaklist)<-c("intensity")
-write.csv(tpeaklist, file="peaklist.csv")
+# print master spectrum
+tiff(filename="masterspectrum.tiff", res=1200, compression="lzw",
+     height=200, width=200,units="mm")
+plot(masterspectrum, xlab="m/z")
+dev.off()
 
-print("Files generated: Pseudo master spectrum (tiff and mzML), spectrum density (tiff), spectrum-zoom with peaks (tiff, peaklist.csv)")
+tiff(filename="spectrum-with-peaks.tiff", res=1200, compression="lzw",
+     height=200, width=200, units="mm")
+plot(masterspectrum, xlim=c(40, 550), xlab="m/z")
+points(peaks, pch=18)
+dev.off()
+
+
+# export a single mzML spectrum
+exportMzMl(masterspectrum, force=TRUE, file="masterspectrum.mzML")
+
+# export of peaks
+exportCsv(peaks, file="peaklist.csv")
+
+message("Files generated: Pseudo master spectrum (tiff and mzML), ",
+        "spectrum density (tiff), ",
+        "spectrum-zoom with peaks (tiff, peaklist.csv)")
