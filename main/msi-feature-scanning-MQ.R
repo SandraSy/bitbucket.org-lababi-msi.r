2d93771f77619a1b7671908bba1533583c36d77b
Dr. Robert Winkler <robert.winkler@ira.cinvestav.mx, robert.winkler@bioprocess.org>
2015-05-14 18:27:40 -0500

    Improved scripts by Sebastian Gibb. Progress bar for feature scanning now with %.

--- main/msi-feature-scanning-MQ.R
+++ main/msi-feature-scanning-MQ.R
@@ -1,6 +1,4 @@
 # This script generates mass images for a selected m/z range and m/z tolerance
-
-#please install msir from https://bitbucket.org/sgibb/msir/ 
 library("msir")
 
 # Load the imzML data
@@ -17,7 +15,8 @@ titles <- sprintf("mz %.1f+/-%.1f", center, tolerance)
 
 col <- rainbow(100L, start=1L/5L)
 
-pb <- txtProgressBar(0L, length(center))
+pb <- txtProgressBar(0L, length(center), style=3)
+
 ## loop through intervals
 for (i in seq(along=center)) {
   png(filenames[i])
@@ -25,13 +24,13 @@ for (i in seq(along=center)) {
                   main=titles[i], xlab="x/ pixel", ylab="y/ pixel",
                   scales=list(draw=FALSE), contour=TRUE, pretty=TRUE,
                   col.regions=col))
-  ## useRaster=TRUE, increases run time dramatically but the visual
+  ## useRaster=TRUE, decreases run time dramatically but the visual
   ## representation changes as well
   #print(levelplot(s[,,i],
   #                main=titles[i], xlab="x/ pixel", ylab="y/ pixel",
   #                scales=list(draw=FALSE), contour=TRUE, pretty=TRUE,
   #                col.regions=col, useRaster=TRUE, interpolate=TRUE))
-  
+
   setTxtProgressBar(pb, i)
   dev.off()
 }
